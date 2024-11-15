ab7fff335119711cb838c417ca43a0f0034372c4
nulluser0 <diuyueng@gmail.com>
2024-11-16 00:25:48 +1100

    Update configuration settings and .gitignore

--- src/config.rs
+++ src/config.rs
@@ -3,8 +3,8 @@ use serde::{Deserialize, Serialize};
 /// Default path to the configuration file.
 pub const DEFAULT_CONFIG_PATH: &str = "./config.toml";
 
-/// The URL of atar.rocks files. Include /files to point to the files directory.
-pub const DEFAULT_URL: &str = "https://atar.rocks/files/";
+/// The URL containing the files. Include paths like `/files/` or `/downloads/` if required to point to the directory containing the files.
+pub const DEFAULT_URL: &str = "https://example.com/files/";
 
 /// The User-Agent header to use for the requests.
 pub const DEFAULT_USER_AGENT: &str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36";
@@ -13,7 +13,7 @@ pub const DEFAULT_USER_AGENT: &str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) 
 pub const DEFAULT_OUTPUT_DIR: &str = "./output";
 
 /// Number of concurrent downloads to perform.
-pub const DEFAULT_CONCURRENT_DOWNLOADS: usize = 16;
+pub const DEFAULT_CONCURRENT_DOWNLOADS: usize = 30;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Config {
